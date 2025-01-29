222ac11ccc6644f069de67c56116e9101d0ea468
Peng <h4peng@ieng6-202.ucsd.edu>
2025-01-29 00:18:00 -0800

    Add linked list

---
+++ list.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node {
+    int item;
+    struct node *next;
+} node;
+
+void free_list(node *first) {
+    node *it = first;
+    while (it != NULL) {
+      free(it);
+      it = it->next;
+    }
+}
+
+void print_list(node *first) {
+    printf("{");
+    for (node *it = first; it != NULL; it = it->next) {
+        if (it != first) printf(",");
+        printf("%d", it->item);
+    }
+    printf("}\n");
+}
+
+node *create_29_list(void) {
+    node *first = malloc(sizeof(node));
+    node *second = malloc(sizeof(node));
+
+    first->item = 2;
+    second->item = 9;
+    first->next = second;
+    second->next = NULL;
+
+    return first;
+}
+
+int main(void) {
+    node *ourlist = create_29_list();
+    print_list(ourlist);
+    ourlist = NULL;
+}
