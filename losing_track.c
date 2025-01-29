91c62e48be55e828ca3830ee59c393c0f2287940
Steven Shi <57511106+2s2e@users.noreply.github.com>
2025-01-28 18:08:50 -0800

    Create losing_track.c

---
+++ losing_track.c
@@ -0,0 +1,32 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *getQueries(char *url) {
+  char *query = malloc(sizeof(char) * (strlen(url) + 1));
+
+  // make our lowercase string
+  for (int i = 0; i < strlen(url); i++) {
+    query[i] = tolower(url[i]);
+  }
+
+  // search for ?
+  char *ptr = query;
+  while (*ptr != '?' && *ptr != 0) {
+    ptr++;
+  }
+  if (*ptr == '?') {
+    return ptr + 1;
+  }
+  return ptr;
+}
+
+int main(int argc, char *argv[]) {
+  char s[] = "https://example.com/over/there?name=ferret";
+
+  char *queries = getQueries(s);
+  printf("%s\n", queries);
+
+  return 0;
+}
